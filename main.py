44b6b897bee4d4d1671375994a28ec0f76f808a0
tosinadedapo <tosinoreka@yahoo.com>
2024-05-23 14:43:00 +0100

    Initial changes

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    #df = pandas.read_csv("")
+    temperature = 23 #df.station(date)
+    return {"station": station,
+            "date": date,
+            "temperature": temperature
+            }
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
